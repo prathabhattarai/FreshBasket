f9144fca09bd3cac53e17334461408cf673aa9cb
Pratha Bhattarai <prathabhattarai2@gmail.com>
2026-06-13 17:32:40 +0545

    fix Vercel build error by removing prerender plugin

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { vitePrerenderPlugin } from "vite-prerender-plugin";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    vitePrerenderPlugin({
-      routes: ["/"],
-    }),
   ],
 });
